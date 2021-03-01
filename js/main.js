ceefa4c2edfb8ad4d7f61c69b1f24b55e968e7bc
zhenshall <67012613+zhenshall@users.noreply.github.com>
2021-03-01 15:12:50 +0000

    Calculating distance for each letter on mousemove

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 $(function() {
 
-    const $letter = $('.letter');
+    const $container = $('.container');
 
-    // Get mouse location and set value as font weight
-    let $x;
-    $letter.mousemove(function(event) {
-        var offset = $(this).offset();
-        $x = event.pageX- offset.left;
-        $(event.currentTarget).css({ fontWeight: $x * 10 });
+    let mX, mY, distance;
+
+    function calculateDistance(elem, mouseX, mouseY) {
+        return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
+    }
+
+    $(document).mousemove(function(e) {
+        
+        $container.children().each(function() {
+            mX = e.pageX;
+            mY = e.pageY;
+            distance = calculateDistance($(this), mX, mY);
+            distance = distance > 900 ? 900 : distance; 
+            $(this).css({ fontWeight: 900 - distance*1.5 });
+        });
+             
     });
-    
-
-    // $letter.on('mouseover', event => {
-    //     $(event.currentTarget).animate({ fontWeight: '600' }, 300);
-    // });
-
-    // $letter.on('mouseleave', event => {
-    //     $(event.currentTarget).animate({ fontWeight: '100' }, 1000);
-    // });
 
 });
