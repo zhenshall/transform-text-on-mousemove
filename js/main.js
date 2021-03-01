718d6272d9f1947a26b1c11119abe844874f5b44
zhenshall <67012613+zhenshall@users.noreply.github.com>
2021-03-01 12:45:05 +0000

    Changing font weight relative to mouse x pos

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ $(function() {
 
     const $letter = $('.letter');
 
-    $letter.on('mouseover', event => {
-        $(event.currentTarget).animate({ fontWeight: '600' }, 300);
+    // Get mouse location and set value as font weight
+    let $x;
+    $letter.mousemove(function(event) {
+        var offset = $(this).offset();
+        $x = event.pageX- offset.left;
+        $(event.currentTarget).css({ fontWeight: $x * 10 });
     });
+    
 
-    $letter.on('mouseleave', event => {
-        $(event.currentTarget).animate({ fontWeight: '100' }, 1000);
-    });
+    // $letter.on('mouseover', event => {
+    //     $(event.currentTarget).animate({ fontWeight: '600' }, 300);
+    // });
+
+    // $letter.on('mouseleave', event => {
+    //     $(event.currentTarget).animate({ fontWeight: '100' }, 1000);
+    // });
 
 });
