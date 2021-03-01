346476e62aec715e1d5bbecdaf52fb80cea95e20
zhenshall <67012613+zhenshall@users.noreply.github.com>
2021-03-01 16:53:18 +0000

    Removing obsolete links and documenting

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ $(function() {
 
     let mX, mY, distance;
 
+    // Returns distance from mouse pointer relative to an element
     function calculateDistance(elem, mouseX, mouseY) {
         return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
     }
 
     $(document).mousemove(function(e) {
         
+        // Calculate distance from mouse pointer for each letter in the container element
         $container.children().each(function() {
             mX = e.pageX;
             mY = e.pageY;
